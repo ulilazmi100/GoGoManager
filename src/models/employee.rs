63c3b083f95c11d9b685726d2cbcd73c4bb65026
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2025-01-11 03:06:40 +0700

    reafctor: use DateTime<utc> for employee times

--- src/models/employee.rs
+++ src/models/employee.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-use chrono::Utc;
+use chrono::{DateTime, Utc};
 
 #[derive(sqlx::FromRow, Serialize, Deserialize, Debug)]
 pub struct Employee {
@@ -10,6 +10,6 @@ pub struct Employee {
     pub employee_image_uri: Option<String>,
     pub gender: String,
     pub department_id: Uuid,
-    pub created_at: chrono::DateTime<Utc>,
-    pub updated_at: chrono::DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
