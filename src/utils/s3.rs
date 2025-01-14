d763702c38f229809c93a32cb085cda6d7ecd8f5
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2025-01-14 10:48:18 +0700

    feat: add behavior to s3 util

--- src/utils/s3.rs
+++ src/utils/s3.rs
@@ -1,10 +1,12 @@
 use aws_sdk_s3::Client as S3Client;
 use aws_config::ConfigLoader;
 use aws_types::region::Region;
+use aws_config::BehaviorVersion; // Import BehaviorVersion
 
 pub async fn create_s3_client() -> S3Client {
     let aws_config = ConfigLoader::default()
         .region(std::env::var("AWS_REGION").ok().map(Region::new))
+        .behavior_version(BehaviorVersion::latest()) // Set behavior version here
         .load()
         .await;
 
